9577716d3e12c85616d8b06b0e3a47d400720365
Anime Sparta <suryagupta1928@gmail.com>
2025-10-20 23:45:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '20478011'))
 API_HASH = environ.get('API_HASH', '0e4dcf39643e83c3c174a0d2370e5b4a')
-BOT_TOKEN = environ.get('BOT_TOKEN', "8432141224:AAFw3qG3Xj6yRc0_ID39Hk0_e-9OxonK9Yc")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8231530189:AAFoz0hdEoVyJZ-iZHNdCcRcfZHy0Mm30xQ")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
@@ -166,5 +166,6 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
